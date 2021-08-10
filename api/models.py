b3a0fe673af72a47c430b49ca04a37c92ed6ae4d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-10 09:23:29 +0900

    修正

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Task(models.model):
+class Task(models.Model):
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- drfapi/settings.py
+++ drfapi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'api.apps.APIConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
